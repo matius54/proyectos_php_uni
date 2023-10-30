6e93d867e6f4251e54d991a57e76d3176652f870
Matius54 <mat54200454@gmail.com>
2023-10-30 14:12:38 -0400

    Update indexes, using for cycles

--- Tema1/index.php
+++ Tema1/index.php
@@ -1,3 +1,16 @@
-<?php
-    echo "hola mundo";
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicios Tema 1</title>
+</head>
+<body>
+    
+    <?php
+    echo "hola mundo, estos son los ejercicios de Tema1";
+    for ($i = 1; $i <= 20 ;$i ++)echo "<br><a href=".$i.".php>Ejercicio ".$i."</a>";
+    ?>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-    echo "hola";
-?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Electiva 2023</title>
+</head>
+<body>
+
+    <?php
+    $temas = 1;
+    for ($i = 1; $i <= $temas ;$i ++)echo "<a href=Tema".$i.">Tema ".$i."</a><br>";
+    echo "hola mundo";
+    ?>
+    
+</body>
+</html>
